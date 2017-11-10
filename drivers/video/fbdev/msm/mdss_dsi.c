b4a941c2aefca1ab4313c02eec0ee23f17e07fee
Joe Maples <joe@frap129.org>
2017-11-09 20:39:16 -0500

    mdss: Add state_notifier hooks
    Signed-off-by: Joe Maples <joe@frap129.org>

--- drivers/video/fbdev/msm/mdss_dsi.c
+++ drivers/video/fbdev/msm/mdss_dsi.c
@@ -776,6 +776,7 @@ static int mdss_dsi_panel_power_ctrl(struct mdss_panel_data *pdata,
 			}
 			ret = mdss_dsi_panel_power_on(pdata);
 		}
+
 #ifdef CONFIG_STATE_NOTIFIER
 		state_resume();
 #endif
